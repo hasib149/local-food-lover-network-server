96ffd50be3586f8e431e9e18c9c7a9b539f3293e
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-11-12 12:38:06 +0600

    server side setup done and mongo db connected successfully

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const port = 3000;
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const uri =
+  "mongodb+srv://localFoodLover:FcZ7franttWdwOqU@cluster0.wxainhe.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
