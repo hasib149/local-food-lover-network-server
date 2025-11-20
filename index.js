34bfe677ad71f9115c58a538197d4619e704060c
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-11-20 13:24:55 +0600

    edit put api make

--- index.js
+++ index.js
@@ -76,6 +76,26 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/reviewUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedReview = req.body;
+
+      const result = await reviewCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedReview }
+      );
+
+      res.send(result);
+    });
+
+    app.get("/reviewUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
+    });
+    
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
